dcbb3788f5b94f881656e34a9f68deffe0214f6c
Дмитрий <123301974+JamaisVu911@users.noreply.github.com>
2023-05-22 21:48:14 +1000

    Создание одномерного числового массива

---
+++ Listing_3._8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Listing_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание массива из 12 чисел:
+            int[] nums = new int[12];
+
+            // Перебор элементов массива:
+            for(int k = 0; k < nums.Length; k++)
+            {
+                // Присваивание значения элементу массива:
+                nums[k] = 3 * k + 1;
+
+                // Отображение значения элемента массива:
+                Console.Write(" | " + nums[k] + " ");
+            }
+
+            Console.WriteLine(" | ");
+            Console.ReadKey();
+        }
+    }
+}
